9ee28ff96931182ea1549825e7ce6b14f6b66584
tastebread <zkal_l@naver.com>
2025-02-13 15:48:13 +0900

    111

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5ql)&+*-yd(vs-br72(#-6mw%bf0boab)nivpv)y%tmc2$m7i%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['myproject-production-9a5c.up.railway.app',"localhost"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
