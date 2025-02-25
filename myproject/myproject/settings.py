4bcef9a8251f085c13bceffee3d18149ced401b8
tastebread <zkal_l@naver.com>
2025-02-25 16:13:01 +0900

    2월 25일 777

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -34,6 +34,11 @@ ALLOWED_HOSTS = [
     '127.0.0.1'
     ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://tastebread.up.railway.app',  # Railway 배포된 도메인
+]
+CSRF_COOKIE_SECURE = True
+
 
 # Application definition
 
